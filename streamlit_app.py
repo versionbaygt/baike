4c7eb43b106ccf6cd5b41b618f47a5af1e3d15e4
Gareth Thomas <gareth@versionbay.com>
2022-04-30 14:25:46 +0200

    feature: adding map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
