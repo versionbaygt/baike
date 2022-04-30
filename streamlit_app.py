c1399659987f1402e82b5ee897dd27c4cd292e07
Gareth Thomas <gareth@versionbay.com>
2022-04-30 18:37:47 +0200

    feat: with RENEST50

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import ImageTk, Image
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image
+import os
+import random
 
 
 map_data = pd.DataFrame(
@@ -28,3 +35,11 @@ picture = st.camera_input("Take a picture")
 
 if picture:
      st.image(picture)
+     # define ResNet50 model
+    model = ResNet50(weights='imagenet')
+    img = image.load_img(picture, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_batch = np.expand_dims(img_array, axis=0)
+    img_preprocessed = preprocess_input(img_batch)
+    prediction = model.predict(img_preprocessed)
+    st.write(f"Ik denk dat ik een {decode_predictions(prediction, top=3)[0][0][1]}. Met {round(decode_predictions(prediction, top=3)[0][0][2]*100)}% zekerheid.")
